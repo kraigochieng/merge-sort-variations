4a9371682574bb1d386daf2875e8a223aaf6e3c2
kraigochieng <kraigochieng@gmail.com>
2023-12-22 12:25:18 +0300

    first commit

---
+++ basic_merge_sort.py
@@ -0,0 +1,33 @@
+import random
+
+def merge_sort(data):
+    if len(data) <= 1:
+        return data
+    mid = len(data) // 2
+    left = merge_sort(data[:mid])
+    right = merge_sort(data[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+# Example usage
+data = list(range(1, 21))
+random.shuffle(data)
+print(f"Unsorted data: {data}")
+sorted_data = merge_sort(data)
+print(f"Sorted data: {sorted_data}")
+
